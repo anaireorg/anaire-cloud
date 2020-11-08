da8b7a72695474ca267186011c1b374fc3029139
Pablo <pablo.montesmoreno@telefonica.com>
2020-11-08 20:43:20 +0100

    Added code to print temperature evolution. Now the percentage the window is left open during class is an argument [0-1]

--- emulate_sensor/emulate_sensor.py
+++ emulate_sensor/emulate_sensor.py
@@ -3,10 +3,18 @@
 from datetime import datetime
 import random
 import time
+import sys
+
+
+if (len(sys.argv) != 2):
+  print('Usage: '+sys.argv[0]+' <percentage 0-1 of opened window during class>')
+  exit()
 
 #general consts
 accelerated = True
 metrics_interval = 30   #How much to wait prior to send a new emulated measurement
+#open_window_percentage_during_class= random.choice([0, 0.3, 0.5, 1])
+open_window_percentage_during_class = float(sys.argv[1])
 
 #CO2 consts
 minimum_CO2 = 450  #minimum C=2 concentration in the room
@@ -14,9 +22,9 @@ CO2_base_increment = random.randint(4,6)  #ppm of CO2 incremented in a minute
 open_window_CO2_decrement = 9 #ppm of CO2 decremented in a minute when a window is opened
 
 #Temperature consts
-minimum_temperature = 15.0
+minimum_temperature = 17.0
 maximum_temperature = 22.0
-temperature_base_increment = 0.05  #Celsius increment per minute during class
+temperature_base_increment = 0.07  #Celsius increment per minute during class
 open_window_temp_decrement = 0.13 #Celsius decrement per minute when the window is opened
 
 #Humidity consts
@@ -36,13 +44,19 @@ schedule = [
   
 def main():
     CO2_increment = CO2_base_increment
+    temperature_increment = temperature_base_increment
+    humidity_decrement = humidity_base_decrement
+    
     #If the class has the door open decrement the per minute CO2 increment
     if random.choice([True,False]): CO2_increment -= 1
      
     #Emulate if the window is opened 30%, 50%, 100% during class
-    CO2_increment -=  int(open_window_CO2_decrement * random.choice([0, 0.3, 0.5, 1]) )
+    CO2_increment -=  int(open_window_CO2_decrement * open_window_percentage_during_class )
+    temperature_increment -= open_window_temp_decrement * open_window_percentage_during_class
+    humidity_decrement += open_window_humidity_increment * open_window_percentage_during_class
 
-    last_value = 0
+    last_CO2_value = 0
+    last_temperature_value = 0
     if accelerated: now = 8*60 + 30  #If accelerated is set emulate to start at 8:30AM
     while (True):
         if accelerated: now += 1    #If accelerated is set emulate increment a minute in each iteration
@@ -59,15 +73,20 @@ def main():
                 break
         
         if class_time:
-            last_value = max(minimum_CO2, last_value + CO2_increment)
-            print('clase:  ' + str(int(now/60))+':'+str(now%60)+'   '+str(CO2_increment))
+            last_CO2_value = max(minimum_CO2, last_CO2_value + CO2_increment)
+            last_temperature_value = min(max(minimum_temperature, last_temperature_value + temperature_increment), maximum_temperature)
+            
+            print('clase:  ' + str(int(now/60))+':'+str(now%60)+'   '+str(open_window_percentage_during_class*100)+'%    '+str(CO2_increment)+'   '+str(temperature_increment))
         else:
-            last_value = max(minimum_CO2, last_value - open_window_CO2_decrement)
-            print('pausa:  ' + str(int(now/60))+':'+str(now%60)+'   '+str(-1 * open_window_CO2_decrement))
+            last_CO2_value = max(minimum_CO2, last_CO2_value - open_window_CO2_decrement)
+            last_temperature_value = min(max(minimum_temperature, last_temperature_value - open_window_temp_decrement), maximum_temperature)
+            
+            print('pausa:  ' + str(int(now/60))+':'+str(now%60)+'   '+str(open_window_percentage_during_class*100)+'%    '+str(-1 * open_window_CO2_decrement)+'   '+str(-1 * open_window_temp_decrement))
               
-        new_value = last_value + random.randint(int(-1 * CO2_base_increment/2), int(CO2_base_increment/2))
+        new_CO2_value = last_CO2_value + random.randint(int(-1 * CO2_base_increment/2), int(CO2_base_increment/2))
+        new_temp_value = last_temperature_value + 0.1 *random.randint(-1, 1)
         
-        print('       '+str(new_value))
+        print('       '+str(new_CO2_value)+'ppm       '+"{:.2f}".format(new_temp_value)+'ºC')
 
         if accelerated: time.sleep(1)     #If accelerated is set repeat iteration every second
         else: time.sleep(metrics_interval)
